302bd7d0bce41f4068ba076c59c342646448052d
Rachel242424 <binijini24@gmail.com>
2022-06-07 17:25:27 +0900

    start

---
+++ sbteam/src/test/java/com/example/sbteam/SbteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
